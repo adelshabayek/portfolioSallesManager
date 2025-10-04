db5f5cd47df1796884ebeeaaf55464f140f31c88
adel shabayek <pildexyt@gmail.com>
2025-10-04 19:48:58 +0300

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit{
   currentYear = new Date().getFullYear();
+
+   constructor(private title: Title, private meta: Meta) {}
+
+  ngOnInit() {
+    this.title.setTitle('About Me | Ehab ElSayed');
+    this.meta.updateTag({ name: 'description', content: 'Learn more about Ehab ElSayed, Sales Director & Business Development Manager with 20+ years experience.' });
+  }
 
   toggleTheme(isDark: boolean) {
     document.body.classList.toggle('dark-theme', isDark);
   }
+
 }
