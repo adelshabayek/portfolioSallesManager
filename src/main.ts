fc20a798cb2628e50b3b78be110d73e4605fa78f
adel shabayek <pildexyt@gmail.com>
2025-10-10 22:09:57 +0300

    szxsZx

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then(() => {
+    const loader = document.getElementById('global-loader');
+    if (loader) {
+      loader.classList.add('fade-out');
+      setTimeout(() => loader.remove(), 600);
+    }
+  })
+  .catch((err) => console.error(err));
